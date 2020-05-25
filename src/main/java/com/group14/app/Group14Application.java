ffcff9376f303a214736cee90bec27c3a21cf960
Sriram Ponangi <sr810178@dal.ca>
2020-05-24 23:04:14 -0300

    initial project setup

---
+++ src/main/java/com/group14/app/Group14Application.java
@@ -0,0 +1,13 @@
+package com.group14.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group14Application.class, args);
+	}
+
+}
