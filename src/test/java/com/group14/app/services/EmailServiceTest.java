bd024a6d60a6293e168678471008af4d915f9027
Siddharth <sd240036@dal.ca>
2020-06-04 15:31:41 -0300

    Test bug

--- src/test/java/com/group14/app/services/EmailServiceTest.java
+++ src/test/java/com/group14/app/services/EmailServiceTest.java
@@ -2,31 +2,70 @@ package com.group14.app.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.springframework.mail.javamail.JavaMailSender;
 
+import com.group14.app.models.Forgotpassword;
 import com.group14.app.repositories.ForgotPasswordRepository;
+
 
 public class EmailServiceTest {
 	
-	ForgotPasswordRepository fPR = new ForgotPasswordRepository();
+	@Mock
+	ForgotPasswordRepository fPR;
 	
-	@Test
-	public void sendMail()
-	{
-		String mail = fPR.readEmail("B00100002");
-		
-		assertNotNull(mail);
-		assertEquals("sd240036@dal.ca", mail);
+	@Mock
+	private JavaMailSender javaMailSender;
+	
+	
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
 	}
 	
+	@InjectMocks
+	EmailService eS;
+	
 	@Test
-	public void sendPass()
+	public void sendMailTest()
 	{
-		String mail = fPR.readPass("B00100002");
+		when(fPR.readEmail(Mockito.anyString())).thenReturn("sd240036@dal.ca");
+		when(fPR.readPass(Mockito.anyString())).thenReturn("xyzpassword");
 		
-		assertNotNull(mail);
-		assertEquals("password", mail);
+		System.out.println(fPR);
+//		Forgotpassword fP = new Forgotpassword(); 
+//		fP.setBanner("B00100002");
+		System.out.println(eS);
+		eS.sendMail(Mockito.any(Forgotpassword.class));
+		
+		
+		verify(fPR.readEmail(Mockito.anyString()));
+		verify(fPR.readPass(Mockito.anyString()));
+		
+//		verify(javaMailSender.send(Mockito.any(SimpleMailMessage.class),times(1)));
+//		assertNotNull(fP.getEmail());
+//		assertNotNull(fP.getPassword());
+//		assertEquals("sd240036@dal.ca", fP.getEmail());
+//		assertEquals("xyzpassword",fP.getPassword());
 	}
+	
+//	@Test
+//	public void sendPass()
+//	{
+//		String mail = fPR.readPass("B00100002");
+//		
+//		assertNotNull(mail);
+//		assertEquals("password", mail);
+//	}
 
 }
