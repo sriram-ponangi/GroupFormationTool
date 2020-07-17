687108231fe5fb930653a72f835c51bfa76fc3d5
Siddharth <sd240036@dal.ca>
2020-07-16 23:36:47 -0300

    Final Commit

---
+++ src/main/java/com/group14/app/models/NumericAnswers.java
@@ -0,0 +1,33 @@
+package com.group14.app.models;
+
+public class NumericAnswers {
+
+	private int response_id;
+	private String student_id;
+	private int answer;
+
+	public int getResponse_id() {
+		return response_id;
+	}
+
+	public void setResponse_id(int i) {
+		this.response_id = i;
+	}
+
+	public String getStudent_id() {
+		return student_id;
+	}
+
+	public void setStudent_id(String student_id) {
+		this.student_id = student_id;
+	}
+
+	public int getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(int answer) {
+		this.answer = answer;
+	}
+
+}
